3f8faa32a36ab903eea273c32c261612a1c1a272
Seb-SOULIER <soulier.sebastien.ss@gmail.com>
2021-06-16 16:16:01 +0200

    product

--- migrations/Version20210611125801.php
+++ migrations/Version20210611125801.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210611102004 extends AbstractMigration
+final class Version20210611125801 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20210611102004 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE newsletter (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE calendar DROP INDEX UNIQ_6EA9A146B6BBB063, ADD INDEX IDX_6EA9A146B6BBB063 (offer_variation_id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE newsletter');
+        $this->addSql('ALTER TABLE calendar DROP INDEX IDX_6EA9A146B6BBB063, ADD UNIQUE INDEX UNIQ_6EA9A146B6BBB063 (offer_variation_id)');
     }
 }
