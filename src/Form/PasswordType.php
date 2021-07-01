0fb78bf1d44c7326df1002766fd022bb7c180789
Le Pober <adrien.le.pober@gmail.com>
2021-07-01 10:00:56 +0200

    add provider edit account

---
+++ src/Form/PasswordType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Provider;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('password', RepeatedType::class, [
+                'type' => \Symfony\Component\Form\Extension\Core\Type\PasswordType::class,
+                'invalid_message' => "Le mot de passe n'est pas le même",
+                'options' => ['attr' => ['class' => 'form-control']],
+                'required' => true,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmation du mot de passe'],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Provider::class,
+        ]);
+    }
+}
