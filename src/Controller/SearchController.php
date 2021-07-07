14dc58ada59c6ad14a323fac563dcd298976be38
Gaetan <gabu-g@hotmail.fr>
2021-07-07 20:05:24 +0200

    add map

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
 use App\Entity\Offer;
+use App\Repository\ContactRepository;
 use App\Service\Api;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -27,6 +29,22 @@ class SearchController extends AbstractController
             'offers' => $offers
         ]);
     }
+//
+//    /**
+//     * @Route("/coordinate", name="coordinate")
+//     */
+//
+//    public function Coordinate(ContactRepository $contactRepository)
+//    {
+//        {
+//            $coordinate = $contactRepository->findAll();
+//            return $this->json($coordinate ?? []);
+//                //'{\"adultes\":3,\"enfants\":4}';
+//            //$this->json($coordinate ?? []);
+//        }
+//    }
+
+
     /**
      * @Route("/localization", name="localization")
      */
@@ -36,7 +54,6 @@ class SearchController extends AbstractController
         $query = $request->query->get('q');
 
         if (null !== $query) {
-
             $url = "https://nominatim.openstreetmap.org/search?q="
             . $query . "&format=json&addressdetails=1&limit=1";
             $localization = $api->getResponse($url);
