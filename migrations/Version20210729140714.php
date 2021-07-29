67c54a3952267a3f1752ef7dda33228cba5d8046
Seb-SOULIER <soulier.sebastien.ss@gmail.com>
2021-07-29 17:03:53 +0200

    fix bug

---
+++ migrations/Version20210729140714.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210729140714 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE offer_variation ADD price_children DOUBLE PRECISION DEFAULT NULL, ADD price_group DOUBLE PRECISION DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE offer_variation DROP price_children, DROP price_group');
+    }
+}
